81327f26cbf2b2ca9b9f0d52eb70ca416a11a5a1
raymondproduction@ukr.net <raymondproduction@ukr.net>
2017-01-30 13:46:35 +0200

    Working tests

--- test/testSpec.js
+++ test/testSpec.js
@@ -6,34 +6,40 @@ define(modules, function(jQ, runner, draw, control) {
     beforeEach(function() {
       // создаем елемент canvas
       // create element canvas
-     el = jQ(" <canvas id='myCanvas' width='640' height='400'></canvas>");
+      el = jQ(" <canvas id='myCanvas' width='640' height='400'></canvas>");
       // кидаем этот виртуальный элемент в DOM
       // push element canvas to DOM
       jQ('body').append(el);
     });
-// Why don't work "this" in draw.js?
-// Why don't work  "toHaveBeenCalled()" for action method
-    it('works runner', function() {
+
+    it('works runner', function(done) {
       var drawTest = {}
+      var test = 0;
       drawTest = {
         init: function() {},
-        action: function() {}
+        action: function() {
+          test = 42;
+        }
       }
       spyOn(drawTest, 'init');
-      spyOn(drawTest, 'action');
+      spyOn(drawTest, 'action').and.callThrough();
       runner.init(drawTest.init, drawTest.action)
       expect(drawTest.init).toHaveBeenCalled();
-      //expect(drawTest.action).toHaveBeenCalled(); //?
-    })
+      var intervalId = setInterval(function() {
+        if (test == 42) {
+          expect(drawTest.action).toHaveBeenCalled();
+          clearInterval(intervalId);
+          done();
+        }
+      }, 10);
 
+    })
 
     it("Test for bricks", function() {
       draw.init();
       draw.initBricks();
       expect(draw.bricks[1][1]).toEqual(1);
     });
-
-
     it("Control default parametr", function() {
       control.init();
       expect(control.canvasMinX).toBeGreaterThan(0);
@@ -41,15 +47,11 @@ define(modules, function(jQ, runner, draw, control) {
       expect(control.rightDown).toEqual(false);
       expect(control.leftDown).toEqual(false);
     });
-
-    it("Listener for control",function(){
-
-      spyOn(jQ(document), 'keydown');
-      //control.init();
-      jQ(document).keydown(controlObj.keyDown)
-       // jQuery('body').keydown();
-       // expect(jQuery.keydown).toHaveBeenCalled();
-       expect(jQ(document).keydown).toHaveBeenCalled();
+    it("Listener for control", function() {
+      spyOn(control, 'keyDown');
+      control.init();
+      jQ(document).trigger("keydown");
+      expect(control.keyDown).toHaveBeenCalled();
     });
 
   });
